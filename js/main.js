7f1d964fe3f8d16e9b02c159131b0f5d390acdcc
rjmcbain <rmcbain01@gmail.com>
2018-08-11 15:28:52 -0600

    adding to js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+// Search Input
+const searchUser = document.getElementById("searchUser");
+
+// Search input event listener
+searchUser.addEventListener("keyup", e => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    console.log(userText);
+  }
+});
